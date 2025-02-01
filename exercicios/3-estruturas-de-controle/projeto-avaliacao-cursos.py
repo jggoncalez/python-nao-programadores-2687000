a03c1729cef9e2321610885cc664892ed920a0d5
João Gabriel Gonçalez <joaogabriel.racer@gmail.com>
2025-02-01 15:09:51 +0000

    resolução

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,32 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+
+cursos = {"Python para não programadores", 
+          "Fundamentos da Gestão de projetos",
+          "Git e Github",
+          "Fundamentos API Rest",
+          "Fundamentos SQL Server" }
+
+cursoGit = "Git e Github"
+cursoCyber = "Cybersegurança"
+cursoPy = "Python para não programadores"
+
+cursoNotas = {}
+
+if cursoGit in cursos:
+  print(f'O curso "{cursoGit}" está na lista de cursos, por favor dê uma avaliação: ')
+  cursoNotas[cursoGit] = int(input("Qual nota você dá para esse curso (de 0 a 5)? "))
+
+if cursoCyber in cursos:
+  print(f'O curso "{cursoCyber}" está na lista de cursos, por favor dê uma avaliação: ')
+  cursoNotas[cursoCyber] = int(input("Qual nota você dá para esse curso (de 0 a 5)? "))
+
+if cursoGit in cursos:
+  print(f'O curso "{cursoPy}" está na lista de cursos, por favor dê uma avaliação: ')
+  cursoNotas[cursoPy] = int(input("Qual nota você dá para esse curso (de 0 a 5)? "))
+
+else:
+  print("Este curso não está disponível na lista de cursos.")
+
+print(cursoNotas)
