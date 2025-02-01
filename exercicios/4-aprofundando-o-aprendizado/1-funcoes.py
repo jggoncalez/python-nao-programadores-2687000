eaca7993a8c46c52d0c361dc42bf1b1247b3fd8c
João Gabriel Gonçalez <joaogabriel.racer@gmail.com>
2025-02-01 16:21:28 +0000

    resolução

--- exercicios/4-aprofundando-o-aprendizado/1-funcoes.py
+++ exercicios/4-aprofundando-o-aprendizado/1-funcoes.py
@@ -1,7 +1,29 @@
 # Crie uma função para selecionar o curso desejado em uma trilha profissional
-
+def curso():
+  curso = int(input('''Selecione o curso desejado:
+                    1. Gestão de projetos
+                    2. Python: formação básica '''))
+  return curso
 # Crie uma função para percorrer todos os níveis de um curso e imprimir na tela a informação do nível atual
+def cursoNivel(cursoSelect):
 
+  trilha = {1: {'title': 'Gestão de projetos', 'nivelFinal': 3 }, 2: {'title': 'Python: formação básica', 'nivelFinal': 5}}
+  
+  cursoAtual = trilha[cursoSelect]['title']
+  nivelAtual = 1
+  nivelFinal = trilha[cursoSelect]['nivelFinal']
+
+  print(f'Bem-vindo ao curso {cursoAtual}, você começará no nível {nivelAtual}.')
+
+  while nivelAtual <= nivelFinal:
+    print(f'Você está no nível {nivelAtual} no {cursoAtual}!')
+    nivelAtual += 1
+
+  else:
+    print(f'Você finalizou o curso {cursoAtual} com sucesso!')
+
+
+          
 # Execute as funções
-
-        
+course = curso()
+cursoNivel(course)
